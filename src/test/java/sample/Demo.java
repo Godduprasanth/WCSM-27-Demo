8530401de2baebc058b834e7636c94bf9d793fae
prasanth <prasanth@prasanth>
2023-04-13 12:05:07 +0530

    commit by AT2

--- src/test/java/sample/Demo.java
+++ src/test/java/sample/Demo.java
@@ -12,7 +12,6 @@ public class Demo {
 	
 		System.out.println("hi ra bavamaridi");
 
-		System.out.println("this is change by AT2");
 
 		System.out.println("Automation test engineer");
 		System.out.println("change made by prasanth");
