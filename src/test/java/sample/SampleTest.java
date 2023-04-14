308c2b0c0521c82aeaa02937ac09f7150a5f387a
prasanth <prasanth@prasanth>
2023-04-14 11:07:46 +0530

    commit by branch

--- src/test/java/sample/SampleTest.java
+++ src/test/java/sample/SampleTest.java
@@ -1,5 +1,15 @@
 package sample;
 
+import org.testng.annotations.Test;
+
 public class SampleTest {
+	
+	@Test
+	public void sample()
+	{
+		System.out.println("This is sample");
+		int a=10,b=15;
+		System.out.println(a+b);
+	}
 
 }
